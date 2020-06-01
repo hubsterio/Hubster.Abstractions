5ee145bcb17b4efe528acc428c0854d0add52b74
codematrix <ross_pellegrino@hotmail.com>
2020-05-31 23:14:26 -0400

    changes

---
+++ Hubster.Abstractions/Models/ContactModel.cs
@@ -0,0 +1,23 @@
+﻿// Hubster 
+// Copyright (c) 2020 Hubster Solutions Inc. All rights reserved.
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hubster.Abstractions.Models
+{
+    public class ContactModel
+    {
+        public ContactModel()
+        {
+            Properties = new List<DirectrPropertyValue>();
+        }
+        
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<DirectrPropertyValue> Properties { get; set; }
+    }
+}
