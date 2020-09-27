2bb3bb16e93ceed6b3824b33071227fb8dd108b0
codematrix <ross_pellegrino@hotmail.com>
2020-09-27 19:05:46 -0400

    Refactoring

---
+++ Hubster.Abstractions/Enums/PacakageServiceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hubster.Abstractions.Enums
+{
+    public enum PacakageServiceType
+    {
+        Internal = 1,
+        Evaluation = 2,
+        Basic = 3,
+        Professional = 4,
+        Premium = 5,
+        Ultima = 6,
+    }
+}
