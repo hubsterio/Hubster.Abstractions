a47e9b28f924b38bff6c1965fda7c261d1e0c349
codematrix <ross_pellegrino@hotmail.com>
2020-09-28 22:40:06 -0400

    Refactoring

--- Hubster.Abstractions/Models/ModelExtensions.cs
+++ Hubster.Abstractions/Models/ModelExtensions.cs
@@ -46,54 +46,6 @@ namespace Hubster.Abstractions.Models
         public static bool IsPaused(this HubModel hub)
         {
             return hub.StatusId == ApplicationType.HubStatus.Paused;
-        }
-
-        /// <summary>
-        /// Determines whether the specified conversation is disabled.
-        /// </summary>
-        /// <param name="conversation">The conversation.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified conversation is disabled; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsDisabled(this ConversationModel conversation)
-        {
-            return conversation.Integration.Hub.Tenant.StatusId == ApplicationType.TenantStatus.Disabled;
-        }
-
-        /// <summary>
-        /// Determines whether the specified integration is disabled.
-        /// </summary>
-        /// <param name="integration">The integration.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified integration is disabled; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsDisabled(this IntegrationModel integration)
-        {
-            return integration.Hub.Tenant.StatusId == ApplicationType.TenantStatus.Disabled;
-        }
-
-        /// <summary>
-        /// Determines whether the specified hub is disabled.
-        /// </summary>
-        /// <param name="hub">The hub.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified hub is disabled; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsDisabled(this HubModel hub)
-        {
-            return hub.Tenant.StatusId == ApplicationType.TenantStatus.Disabled;
-        }
-
-        /// <summary>
-        /// Determines whether the specified tenant is disabled.
-        /// </summary>
-        /// <param name="tenant">The tenant.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified tenant is disabled; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsDisabled(this TenantModel tenant)
-        {
-            return tenant.StatusId == ApplicationType.TenantStatus.Disabled;
-        }
+        }      
     }
 }
