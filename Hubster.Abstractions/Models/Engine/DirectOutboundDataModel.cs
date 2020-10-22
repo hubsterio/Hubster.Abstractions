4f72eda16341e4c5a66d2f2ddb38500c5beaed2e
codematrix <ross_pellegrino@hotmail.com>
2020-10-22 15:30:19 -0400

    Refactor

---
+++ Hubster.Abstractions/Models/Engine/DirectOutboundDataModel.cs
@@ -0,0 +1,31 @@
+﻿// Hubster 
+// Copyright (c) 2020 Hubster Solutions Inc. All rights reserved.
+
+using Hubster.Abstractions.Models;
+using Hubster.Abstractions.Models.Direct;
+using Newtonsoft.Json;
+using System;
+
+namespace Hubster.Abstractions.Models.Engine
+{
+    public class DirectOutboundDataModel
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? HubId { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? TenantId { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? IntegrationId { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? ConversationId { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public ConversationPropertiesModel ConversationProperties { get; set; }
+        
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public DirectActivityModel Activity { get; set; }
+    }
+}
